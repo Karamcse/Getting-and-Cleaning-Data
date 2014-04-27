006f7da1f2b29a8b26db6a0835363fb0bbce3f7c
Karamcse <Karamcse@github.com>
2014-04-27 21:10:20 +0530

    getting and cleaning data assignement

---
+++ run_Analysis.R
@@ -0,0 +1,13 @@
+getwd()
+setwd("j:/Coursera/R/getting and cleaning assignement data/UCI HAR Dataset/")
+activity_label<-read.table("activity_labels.txt")
+activity_label
+features<-read.table("features.txt")
+features
+subject_test<-read.table("./test/subject_test.txt")
+xtest<-read.table("./test/X_test.txt")
+ytest<-read.table("./test/y_test.txt")
+
+subject_train<-read.table("./train/subject_train.txt")
+xtrain<-read.table("./train/X_train.txt")
+ytrain<-read.table("./train/y_train.txt")
